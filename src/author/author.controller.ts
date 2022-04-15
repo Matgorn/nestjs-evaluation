27bb56b1e30c854ed3721d6b1370e80a83eae724
matgorn <m.gorny@selleo.com>
2022-04-15 12:37:41 +0200

    Add redirect

--- src/author/author.controller.ts
+++ src/author/author.controller.ts
@@ -5,6 +5,7 @@ import {
   Param,
   Post,
   Put,
+  Res,
   UseGuards,
 } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
@@ -16,13 +17,18 @@ import { AuthorDTO } from './dto/author.dto';
 import { Author } from './entities/author.entity';
 
 @Controller('authors')
-@UseGuards(JwtAuthGuard, RolesGuard)
+// @UseGuards(JwtAuthGuard, RolesGuard)
 export class AuthorsController {
   constructor(private authorsService: AuthorsService) {}
 
   @Post()
   create(@Body() authorDto: AuthorDTO) {
     return this.authorsService.create(authorDto);
+  }
+
+  @Post('test')
+  testCreate(@Res() res) {
+    return res.redirect('/authors');
   }
 
   @Put(':id')
