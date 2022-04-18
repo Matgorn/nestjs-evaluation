4a82185ae57d74c2b83f0d89025a37bfc6e25ab5
matgorn <m.gorny@selleo.com>
2022-04-18 17:06:44 +0200

    Add asynchronous provider to init roles if not exist

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,5 +1,6 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
 
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
@@ -8,6 +9,7 @@ import { RolesModule } from 'src/role/role.module';
 import { RoleEntity } from 'src/role/entities/role.entity';
 import { MailModule } from 'src/mail/mail.module';
 import { NotificationModule } from 'src/notification/notification.module';
+import { Role } from 'src/role/role.types';
 
 @Module({
   imports: [
@@ -16,7 +18,36 @@ import { NotificationModule } from 'src/notification/notification.module';
     forwardRef(() => MailModule),
     NotificationModule,
   ],
-  providers: [UserService],
+  providers: [
+    {
+      provide: UserService,
+      useFactory: async (connection: Connection) => {
+        let userRole = await connection.manager.findOne(RoleEntity, {
+          where: { name: Role.User },
+        });
+        let adminRole = await connection.manager.findOne(RoleEntity, {
+          where: { name: Role.Admin },
+        });
+
+        if (!userRole) {
+          userRole = await connection.manager.create(RoleEntity, {
+            name: Role.User,
+          });
+
+          await connection.manager.save(RoleEntity, userRole);
+        }
+
+        if (!adminRole) {
+          adminRole = await connection.manager.create(RoleEntity, {
+            name: Role.Admin,
+          });
+
+          await connection.manager.save(RoleEntity, adminRole);
+        }
+      },
+      inject: [Connection],
+    },
+  ],
   controllers: [UserController],
   exports: [UserService],
 })
